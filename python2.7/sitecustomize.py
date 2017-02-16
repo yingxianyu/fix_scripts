49b71a49e9b5467e19f459942903451d19f4adeb
root <root@needu.top>
2017-02-16 13:59:42 +0800

    update sitecustomize.py

--- python2.7/sitecustomize.py
+++ python2.7/sitecustomize.py
@@ -3,3 +3,9 @@ import sys
 
 reload(sys)
 sys.setdefaultencoding('utf8')
+
+
+'''
+    此脚本放置到：/usr/lib64/python2.7/site-packages/
+    解决：UnicodeDecodeError: 'ascii' codec can't decode byte 0xe4 in position 0: ordinal not in range(128) 的错误
+'''
